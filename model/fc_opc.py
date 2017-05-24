d5904a940bcb1d593034ca53e032408a73959593
Tom Seago <tom@tomseago.com>
2017-05-23 23:15:02 -0700

    Don't do verbose debug

--- model/fc_opc.py
+++ model/fc_opc.py
@@ -24,8 +24,8 @@ class FCOPCModel(object):
     def __init__(self, server_ip_port="localhost:7890", debug=False, max_pixels=512, filename="data/opc_mapping.json"):
         print "Connecting to OPC server %s, max_pixels=%d" % (server_ip_port, max_pixels)
 
-        # long lived connections and verbose debug
-        self.opc = opc.Client(server_ip_port, True, True)
+        # long lived connections and not verbose debug
+        self.opc = opc.Client(server_ip_port, True, False)
 
         # Load a pixel mapping from "panel name" format to channel & ix
         with open(filename) as f:
